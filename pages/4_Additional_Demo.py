96e351c17c58d3514a6cf803eefd8b9cf4a25b80
ansza <137867309+ansza@users.noreply.github.com>
2023-09-26 14:45:07 +0000

    end

--- pages/4_Additional_Demo.py
+++ pages/4_Additional_Demo.py
@@ -39,3 +39,6 @@ hour_to_filter = st.slider('hour', 0, 23, 17)
 filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
 st.subheader(f'Map of all pickups at {hour_to_filter}:00')
 st.map(filtered_data)
+
+
+st.subheader('-end-')
